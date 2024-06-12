09dcfb7bebf49c8d30f3b5544dde51bcbcc1a102
sucaroque <roque.ect@gmail.com>
2024-06-12 18:23:51 -0500

    Implementacion swagger

--- src/main/java/com/codigo/swagger/controller/PersonaController.java
+++ src/main/java/com/codigo/swagger/controller/PersonaController.java
@@ -2,6 +2,12 @@ package com.codigo.swagger.controller;
 
 import com.codigo.swagger.entity.PersonaEntity;
 import com.codigo.swagger.service.PersonaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +20,10 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/persona")
+@Tag(
+        name = "API DE MANTENIMIENTO PERSONAS",
+        description = "Esta api contiene todos los endPoint para el mantenimiento de la entidad persona."
+)
 public class PersonaController {
     @Qualifier("personaServiceImpl")
     @Autowired
@@ -24,11 +34,29 @@ public class PersonaController {
     private PersonaService personaService2;
 
     @PostMapping()
+    @Operation(summary = "Crea una Persona")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Persona creada exitosamente",
+                    content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PersonaEntity.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request, el request no cumple con lo esperado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonaEntity.class))})
+    })
     public ResponseEntity<PersonaEntity> crear(@RequestBody PersonaEntity personaEntity) {
         return ResponseEntity.ok(personaService.crear(personaEntity));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar una Persona")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Persona encontrada",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonaEntity.class))}),
+            @ApiResponse(responseCode = "404", description = "Not Found, Persona no encontrada",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PersonaEntity.class))})
+    })
     public ResponseEntity<Optional<PersonaEntity>> buscarPorId(@PathVariable long id) {
         return ResponseEntity.ok(personaService2.buscarPorId(id));
     }
